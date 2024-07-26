1bc5f2461a8a3c8294157768ef7cf687d3228a40
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-07-26 21:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
-  // your code here
+  // your code her
+	const result='';
+	if(s!==''){
+		for(let i=0;i<s.length;i++){
+			if(s.charAt(i)===' '){
+				return result;
+			}
+			else{
+							result+=s.charAt(i);
+			}
+			}
+		return result;
+		}
+	}
 }
 
 // Do not change the code below
